8a030716566b3fd8aecb289eb662d25638f4b3e2
Manuel Garcia Molledo <mgmolledo@gmail.com>
2026-03-04 14:35:43 +0100

    Update model version in answer.js

--- tribunal_vercel/api/answer.js
+++ tribunal_vercel/api/answer.js
@@ -170,7 +170,7 @@ module.exports = async (req, res) => {
 
   try {
     const msg = await client.messages.create({
-      model: 'claude-haiku-4.5',
+      model: 'claude-haiku-4-5-20251001',
       max_tokens: 200,
       system: SYSTEM,
       messages: [{ role: 'user', content: question }]
